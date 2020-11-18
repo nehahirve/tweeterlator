43de03c2759fb539bf69cb419061195b2d8aedb3
nehahirve <neha.s.hirve@gmail.com>
2020-11-18 09:35:55 +0100

    testing

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,11 @@
 import React from "react"
 import Header from "./components/header"
 
-export default function Home() {
+export default function About() {
   return (
     <>
       <Header />
-      <div>this is the main page</div>
+      <div>About us</div>
     </>
   )
 }
