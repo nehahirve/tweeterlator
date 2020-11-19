fdf1b1023a09c786d7ec14a31aeb61af41c79258
nehahirve <neha.s.hirve@gmail.com>
2020-11-19 12:00:34 +0100

    loaded svg map

--- src/components/map.js
+++ src/components/map.js
@@ -1,6 +1,12 @@
 import React from "react"
 import ButtonList from "../components/button_list"
+import lines from "../../static/SVG/lines.svg"
 
 export default function Map() {
-  return <ButtonList stations={["Akalla", "Kista", "Husby", "Alvik"]} />
+  return (
+    <div className="background-map">
+      <img src={lines} alt="" />
+      <ButtonList stations={["Akalla", "Kista", "Husby", "Alvik"]} />
+    </div>
+  )
 }
