92e6e28175b58bba0f0decdc5bee04102d55cb78
nehahirve <neha.s.hirve@gmail.com>
2020-11-20 11:33:39 +0100

    console logged data - lovisa

--- static/fetchdata.js
+++ static/fetchdata.js
@@ -1,120 +1,58 @@
-const https = require("follow-redirects").https
+const fetch = require("node-fetch")
 const fs = require("fs")
 
 const radius = 5
-
 const cities = [
   { name: "stockholm", lat: 59.33258, lon: 18.0649 },
   { name: "malmö", lat: 55.60587, lon: 13.00073 },
-  { name: "umeå", let: 63.82842, lon: 20.25972 },
+  { name: "umeå", lat: 63.82842, lon: 20.25972 },
   { name: "sundsvall", lat: 62.39129, lon: 17.3063 },
   { name: "göteborg", lat: 57.650002, lon: 12.016667 },
 ]
 
-let options, maxId
+let maxId, url
 
-function fetchData(city, maxId, i) {
-  // set options 
-  // Fetch 100 tweets from one city
-  options = {
-    method: "GET",
-    hostname: "api.twitter.com",
-    path: `/1.1/search/tweets.json?q=geocode%3A${city.lat}%2C${city.lon}%2C${radius}km&count=100&max_id=${maxId}`,
-    headers: {
-      Authorization:
-        "Bearer AAAAAAAAAAAAAAAAAAAAAL95JwEAAAAA%2BHXhTUoCMMZevQt2kq0qMTwuDkQ%3D32LFyGWm49UV3oz3R3xNld4IqDTe1ua4ZkRcAzFerCQV2wgYMe",
-      Cookie:
-        'personalization_id="v1_LT8r3+BhOs+k4ZsQDiqyEw=="; guest_id=v1%3A160570801728849585',
-    },
-    maxRedirects: 20,
-  }
-
-  let req = https.request(options, function (res) {
-    let chunks = []
-
-    res.on("data", function (chunk) {
-      chunks.push(chunk)
-    })
-
-    res.on("end", function (chunk) {
-      let body = Buffer.concat(chunks)
-      fs.writeFileSync(
-        `${city.name}_${i}.json`,
-        JSON.stringify(body.toString())
-      )
-      return ('hello');
-    })
-
-    res.on("error", function (error) {
-      console.error(error)
-    })
-  })
-  
-  req.end()
-  return ('hi');
+let options = {
+  method: "GET",
+  headers: {
+    Authorization:
+      "Bearer AAAAAAAAAAAAAAAAAAAAAL95JwEAAAAA%2BHXhTUoCMMZevQt2kq0qMTwuDkQ%3D32LFyGWm49UV3oz3R3xNld4IqDTe1ua4ZkRcAzFerCQV2wgYMe",
+    Cookie:
+      'personalization_id="v1_LT8r3+BhOs+k4ZsQDiqyEw=="; guest_id=v1%3A160570801728849585',
+  },
 }
 
-async function getData() {
+function getDataForAllCities(cities) {
   for (let city of cities) {
-    console.log(fetchData (city, 0, 1));
-   // await fetchData ()
+    maxId = 0
+    let lat = city.lat
+    let lon = city.lon
+    get100sOfTweets(maxId, lat, lon, 5)
   }
 }
 
-// function getData() {
-//   let text
-//   for (let city of cities) {
-//     for (let i = 0; i < 5; i++) {
-//       maxId = 0
+async function get100sOfTweets(maxId, lat, lon, number) {
+  for (let i = 0; i < number; i++) {
+    url = `https://api.twitter.com/1.1/search/tweets.json?q=geocode:${lat},${lon},${radius}km&count=100&max_id=${maxId}`
+    await get100Tweets(url, options)
+  }
+}
 
-//       options = {
-//         method: "GET",
-//         hostname: "api.twitter.com",
-//         path: `/1.1/search/tweets.json?q=geocode%3A${city.lat}%2C${city.lon}%2C${radius}km&count=100&max_id=${maxId}`,
-//         headers: {
-//           Authorization:
-//             "Bearer AAAAAAAAAAAAAAAAAAAAAL95JwEAAAAA%2BHXhTUoCMMZevQt2kq0qMTwuDkQ%3D32LFyGWm49UV3oz3R3xNld4IqDTe1ua4ZkRcAzFerCQV2wgYMe",
-//           Cookie:
-//             'personalization_id="v1_LT8r3+BhOs+k4ZsQDiqyEw=="; guest_id=v1%3A160570801728849585',
-//         },
-//         maxRedirects: 20,
-//       }
+async function get100Tweets(url, options) {
+  const response = await fetch(url, options)
+  const data = await response.json()
+  //console.log(data)
+  maxId = getMaxID(data) // reset MaxID
+  console.log(data)
+}
 
-//       let req = https.request(options, function (res) {
-//         let chunks = []
+// HELPER FUNCTIIONS
+function getMaxID(data) {
+  nextResultsString = data.search_metadata.next_results
+  const maxId = nextResultsString.match(/\d+/)[0]
+  return maxId
+}
 
-//         res.on("data", function (chunk) {
-//           chunks.push(chunk)
-//         })
+// INIT
 
-//         res.on("end", function (chunk) {
-//           let body = Buffer.concat(chunks)
-          
-//           //text = text + body.toString()
-//           //console.log(text)
-//           fs.writeFileSync(
-//             `${city.name}_${i + 1}.json`,
-//             JSON.stringify(body.toString())
-//           )
-//         })
-
-//         res.on("error", function (error) {
-//           console.error(error)
-//         })
-//       })
-
-//       req.end()
-//     }
-//   }
-// }
-
-getData()
-
-
-          // let workingData = JSON.parse(body.toString())
-          // let maxIdString = workingData.search_metadata.next_results
-          // if (maxIdString) {
-          //   nextMaxId = maxIdString.match(/\d+/)
-          //   maxId = nextMaxId[0]
-          //   console.log(maxId)
-          // }
+getDataForAllCities(cities)
