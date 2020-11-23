cd15142215a59535bbd840861fedf291aac0eab4
nehahirve <neha.s.hirve@gmail.com>
2020-11-23 16:24:12 +0100

    playng with cytoscape

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,27 +1,49 @@
 import React from "react"
-import { ResponsiveNetwork } from "@nivo/network"
+import Cytoscape from "cytoscape"
+import fcose from "cytoscape-fcose"
+import CytoscapeComponent from "react-cytoscapejs"
 
-export default function Graph(props) {
-  console.log(props.data)
-  const data = { ...props.data.nodes }
-  return (
-    <div style={{ height: "400px" }}>
-      <ResponsiveNetwork
-        data={data}
-        margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        repulsivity={6}
-        iterations={60}
-        nodeColor={function (e) {
-          return e.color
+import data from "../../static/data.json"
+
+Cytoscape.use(fcose)
+
+export default class Graph extends React.Component {
+  constructor(props) {
+    super(props)
+    this.ready = this.ready.bind(this)
+  }
+
+  ready() {
+    console.log("hello")
+  }
+
+  render() {
+    const layout = { name: "random" }
+
+    return (
+      <CytoscapeComponent
+        className="cy"
+        elements={data}
+        layout={layout}
+        stylesheet={[
+          {
+            selector: "node",
+            style: {
+              shape: "ellipse",
+            },
+          },
+          {
+            selector: "edge",
+            style: {
+              width: 1,
+            },
+          },
+        ]}
+        cy={cy => {
+          this.cy = cy
         }}
-        nodeBorderWidth={1}
-        nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
-        linkThickness={function (e) {
-          return 2 * (2 - e.source.depth)
-        }}
-        motionStiffness={160}
-        motionDamping={12}
+        onClick={() => this.ready()}
       />
-    </div>
-  )
+    )
+  }
 }
--- src/pages/neha.js
+++ src/pages/neha.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "../components/header"
+import Graph from "../components/graph"
 
 export default function Neha() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <Graph />
+      </main>
     </>
   )
 }
