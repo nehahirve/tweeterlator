1f95643de074addd30633ff0507ab05d961e5629
Omid <omid.haqbin@gmail.com>
2020-11-24 10:04:18 +0100

    svg map and stuff

--- src/pages/lovisa.js
+++ src/pages/lovisa.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "../components/header"
+import SwedenMap from "../components/sweden"
 
 export default function Lovisa() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <SwedenMap />
+      </main>
     </>
   )
 }
--- src/pages/omid.js
+++ src/pages/omid.js
@@ -1,11 +1,14 @@
 import React from "react"
 import Header from "../components/header"
+import BubbleChart from "../components/bubbleChart"
 
 export default function Omid() {
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <BubbleChart />
+      </main>
     </>
   )
 }
