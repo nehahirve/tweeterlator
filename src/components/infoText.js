8b81b3607e96249b1f2b763a0c3a5db9166c1be7
Omid <omid.haqbin@gmail.com>
2020-11-25 10:29:03 +0100

    not complete toggle

--- src/components/infoText.js
+++ src/components/infoText.js
@@ -1,13 +1,30 @@
 import React from "react"
 
-export default function Info () {
+export default class Info extends React.Component () {
+
+    state = { isActive: false };
+
+    handleToggle = () => {
+        this.setState({ isActive: !this.state.isActive });
+    };
+
     function toggleInfo(e) {
         e.preventDefault();
         console.log('The link was clicked.');
       }
+
+    render() {
+        const isActive = this.state.isActive;
+        return (
+          <div className={isActive ? "app" : null}>
+            <h1>Hello react</h1>
+            <button onClick={this.handleToggle}>Toggle class</button>
+          </div>
+        );
+    }
     return(
         <>
-        <button className="info" onClick={toggleInfo}>About</button>
+        <button className="info" onClick={this.toggleInfo}>About</button>
         <div className="info-text">
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, culpa
