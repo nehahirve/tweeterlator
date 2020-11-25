77350883031a86b8d6e6c3da19b768a47cc2bc18
Omid <omid.haqbin@gmail.com>
2020-11-25 10:48:51 +0100

    toggle btn info

--- src/components/infoText.js
+++ src/components/infoText.js
@@ -1,31 +1,20 @@
 import React from "react"
 
-export default class Info extends React.Component () {
-
+export default class Info extends React.Component {
     state = { isActive: false };
 
-    handleToggle = () => {
+    toggleInfoClass = () => {
         this.setState({ isActive: !this.state.isActive });
     };
 
-    function toggleInfo(e) {
-        e.preventDefault();
-        console.log('The link was clicked.');
-      }
 
-    render() {
+    render() {  
         const isActive = this.state.isActive;
         return (
-          <div className={isActive ? "app" : null}>
-            <h1>Hello react</h1>
-            <button onClick={this.handleToggle}>Toggle class</button>
-          </div>
-        );
-    }
-    return(
         <>
-        <button className="info" onClick={this.toggleInfo}>About</button>
-        <div className="info-text">
+        
+        <button className="infoBtn" onClick={this.toggleInfoClass}>About</button>
+        <div className={`info-text ${isActive ? "show" : null}`}>
             <p>
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, culpa
                 eaque porro rerum voluptate, obcaecati sapiente cumque saepe sit
@@ -42,4 +31,5 @@ export default class Info extends React.Component () {
         </div>
         </>
     )
+        }
 }
