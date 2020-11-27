a41cc1ea387526826f907dddb935639d7e1bdb30
Lovisa Elensky <lovisa@Lovisas-MacBook-Pro-2.local>
2020-11-27 15:27:34 +0100

    changed step3_vis so it's writing all the data from all the cities into data_vis

--- static/step3_vis.js
+++ static/step3_vis.js
@@ -3,112 +3,132 @@ const MoxyTA = require('moxy-ta') // word frequency analyser
 const Markov = require('ez-markov') // creates a markov graph
 const fs = require('fs')
 const sw = require('stopword') // removes common words
+const data = {}
 
-// READ THE TEXT, TRIM AND CLEAN IT
-let text = fs.readFileSync('TEXTDATA/stockholm.txt').toString().toLowerCase()
+const cities = [
+  { name: 'stockholm', lat: 59.33258, lon: 18.0649 },
+  { name: 'malmö', lat: 55.60587, lon: 13.00073 },
+  { name: 'umeå', lat: 63.82842, lon: 20.25972 },
+  { name: 'sundsvall', lat: 62.39129, lon: 17.3063 },
+  { name: 'göteborg', lat: 57.650002, lon: 12.016667 },
+  // { name: 'karlstad', lat: 59.3793, lon: 13.50357 },
+  // { name: 'kiruna', lat: 67.85572, lon: 20.22513 },
+]
 
-let condensedText = TextCleaner(text).condense().valueOf()
+function generateGraphForAllCities(cities) {
+  for (let city of cities) {
+    generateGraph(city)
+  }
+  fs.writeFileSync('data_vis.json', JSON.stringify(data))
+}
+generateGraphForAllCities(cities)
 
-// REMOVE STOPWORDS
-let customStopWords = ['rt', ',', '=', '-', '--', "'"]
+function generateGraph(city) {
+  let text = fs
+    .readFileSync(`TEXTDATA/${city.name}.txt`)
+    .toString()
+    .toLowerCase()
+  let condensedText = TextCleaner(text).condense().valueOf()
 
-let swedish = sw.removeStopwords(condensedText.split(' '), sw.sv)
-let english = sw.removeStopwords(swedish, sw.en)
-let custom = sw.removeStopwords(english, customStopWords).join(' ')
+  // REMOVE STOPWORDS
+  let customStopWords = ['rt', ',', '=', '-', '--', "'"]
 
-// GETTING WORD FREQUENCY DATA
-const ta = new MoxyTA(custom)
-let frequencyData = ta.scan().wordFrequency
+  let swedish = sw.removeStopwords(condensedText.split(' '), sw.sv)
+  let english = sw.removeStopwords(swedish, sw.en)
+  let custom = sw.removeStopwords(english, customStopWords).join(' ')
+  const ta = new MoxyTA(custom)
+  let frequencyData = ta.scan().wordFrequency
 
-// CREATE MARKOV CHAIN
-const chain = new Markov()
+  // CREATE MARKOV CHAIN
+  const chain = new Markov()
 
-chain.addCorpus(custom)
-const graph = chain.export()
+  chain.addCorpus(custom)
+  const graph = chain.export()
 
-// FILTER OUT THE GRAPH TO ONLY INCLUDE TOP WORDS
+  // FILTER OUT THE GRAPH TO ONLY INCLUDE TOP WORDS
 
-function createDictionaryofTopWords(threshold) {
-  let topWords = {}
-  for (let [key, value] of Object.entries(frequencyData)) {
-    if (value.frequency > threshold) {
-      // threshold for top words
-      topWords[key] = value.frequency
+  function createDictionaryofTopWords(threshold) {
+    let topWords = {}
+    for (let [key, value] of Object.entries(frequencyData)) {
+      if (value.frequency > threshold) {
+        // threshold for top words
+        topWords[key] = value.frequency
+      }
     }
+    return topWords
   }
-  return topWords
-}
 
-const topWords = createDictionaryofTopWords(3)
+  const topWords = createDictionaryofTopWords(3)
 
-const newNodes = []
+  const newNodes = []
 
-for (let node of graph.nodes) {
-  if (topWords.hasOwnProperty(node.label)) {
-    //console.log(node.label)
-    newNodes.push(node)
-  }
-}
-
-for (let node of newNodes) {
-  node.value = topWords[node.label]
-}
-
-graph.nodes = newNodes
-
-// at this point we have filtered out nodes in graph
-
-let newEdges = []
-let evenMoreNewEdges = []
-for (let edge of graph.edges) {
   for (let node of graph.nodes) {
-    if (node.id === edge.from) {
-      newEdges.push(edge)
-      break
+    if (topWords.hasOwnProperty(node.label)) {
+      //console.log(node.label)
+      newNodes.push(node)
     }
   }
-}
 
-for (let edge of newEdges) {
+  for (let node of newNodes) {
+    node.value = topWords[node.label]
+  }
+
+  graph.nodes = newNodes
+
+  // at this point we have filtered out nodes in graph
+
+  let newEdges = []
+  let evenMoreNewEdges = []
+  for (let edge of graph.edges) {
+    for (let node of graph.nodes) {
+      if (node.id === edge.from) {
+        newEdges.push(edge)
+        break
+      }
+    }
+  }
+
+  for (let edge of newEdges) {
+    for (let node of graph.nodes) {
+      if (node.id === edge.to) {
+        evenMoreNewEdges.push(edge)
+        break
+      }
+    }
+  }
+
+  graph.edges = evenMoreNewEdges
+
+  // add to cytodata structure
+  const visData = {
+    nodes: [],
+    edges: [],
+  }
+
   for (let node of graph.nodes) {
-    if (node.id === edge.to) {
-      evenMoreNewEdges.push(edge)
-      break
+    let dataObj = {
+      id: node.id,
+      value: node.value,
+      label: node.label,
     }
+    visData.nodes.push(dataObj)
   }
-}
 
-graph.edges = evenMoreNewEdges
+  let counter = 0
 
-// add to cytodata structure
-const visData = {
-  nodes: [],
-  edges: [],
-}
+  for (let edge of graph.edges) {
+    counter++
 
-for (let node of graph.nodes) {
-  let dataObj = {
-    id: node.id,
-    value: node.value,
-    label: node.label,
+    let dataObj = {
+      from: edge.from,
+      to: edge.to,
+      value: edge.weight,
+      title: `edge-${counter}`,
+    }
+    visData.edges.push(dataObj)
   }
-  visData.nodes.push(dataObj)
+  console.log(city)
+  data[city.name] = visData
 }
 
-let counter = 0
-
-for (let edge of graph.edges) {
-  counter++
-
-  let dataObj = {
-    from: edge.from,
-    to: edge.to,
-    value: edge.weight,
-    title: `edge-${counter}`,
-  }
-  visData.edges.push(dataObj)
-}
-
-console.log(visData.nodes.length)
-
-fs.writeFileSync('data_vis.json', JSON.stringify(visData))
+// fs.writeFileSync('data_vis.json', JSON.stringify(visData))
