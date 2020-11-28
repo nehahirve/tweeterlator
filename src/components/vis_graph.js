aa2cbf0198cc95bdbfa359798ebf222e86c74c6e
nehahirve <neha.s.hirve@gmail.com>
2020-11-28 16:45:57 +0100

    mad a class component, put graph in state

--- src/components/vis_graph.js
+++ src/components/vis_graph.js
@@ -3,80 +3,118 @@ import Graph from 'react-graph-vis'
 import data from '../../static/data_vis.json'
 //import 'react-vis/dist/style.css'
 
-export default function VisGraph(props) {
-  const graph = data[props.station.toLowerCase()]
+export default class VisGraph extends React.Component {
+  constructor(props) {
+    super(props)
+    this.network = React.createRef()
+    this.selectLabel = this.selectLabel.bind(this)
+    this.initNetwork = this.initNetwork.bind(this)
+    this.options = {
+      autoResize: true,
+      width: '100%',
+      height: '100%',
+      nodes: {
+        borderWidth: 0,
+        borderWidthSelected: 0,
+        chosen: {
+          label: this.selectLabel,
+        },
+        color: { color: 'red', highlight: 'blue' },
+        font: { color: 'green', size: 40, face: 'IBM Plex Mono' },
+        shape: 'dot',
+      },
+      edges: {
+        arrows: {
+          to: { enabled: false },
+        },
+        color: {
+          color: '#848484',
+          highlight: 'red',
+          hover: '#848484',
+          inherit: 'from',
+          opacity: 1.0,
+        },
+      },
+      layout: {
+        randomSeed: 12979,
+        improvedLayout: true,
+        clusterThreshold: 150,
+      },
+      physics: {
+        enabled: true,
+        solver: 'repulsion',
+        wind: {
+          x: 1000000,
+        },
+      },
+    }
+    this.state = {
+      currentCity: this.props.station,
+      graph: data[this.props.station.toLowerCase()],
+    }
+  }
 
-  function selectLabel(values) {
+  initNetwork(network) {
+    console.log('changed')
+    network.moveTo({
+      scale: 0.5,
+      position: { x: 0, y: 0 },
+      offset: { x: 300, y: 0 },
+    })
+  }
+
+  componentDidMount() {
+    this.setState({ currentCity: this.props.station })
+    this.setState({ graph: data[this.props.station.toLowerCase()] })
+    this.initNetwork(this.network)
+  }
+
+  componentDidUpdate() {
+    if (this.props.station !== this.state.currentCity) {
+      this.setState({ currentCity: this.props.station })
+      this.setState({ graph: data[this.props.station.toLowerCase()] })
+      this.initNetwork(this.network)
+    }
+  }
+
+  selectLabel(values) {
     values.color = 'yellow'
   }
 
-  const cityNode = {
-    id: 0,
-    label: 'THIS IS STOCKHOLM',
-    x: 0,
-    y: 0,
-    value: 300,
-    fixed: true,
-    physics: false,
-  }
+  render() {
+    const node = {
+      id: 0,
+      label: 'THIS IS STOCKHOLM',
+      x: 0,
+      y: 0,
+      value: 100000,
+      fixed: true,
+      physics: false,
+    }
+    const edge = {
+      from: 0,
+      to: this.state.graph.nodes[0].id,
+      title: 'THIS IS THE EDGE',
+      value: 5,
+    }
 
-  const cityEdge = {
-    from: 0,
-    to: graph.nodes[0].id,
-    title: 'THIS IS THE EDGE',
-    value: 5,
-  }
-
-  graph.nodes.push(cityNode)
-  graph.edges.push(cityEdge)
-
-  const options = {
-    autoResize: true,
-    width: '100%',
-    height: '100%',
-    nodes: {
-      borderWidth: 0,
-      borderWidthSelected: 0,
-      chosen: {
-        label: selectLabel,
+    const events = {
+      select: function (event) {
+        var { nodes, edges } = event
       },
-      color: { color: 'red', highlight: 'blue' },
-      font: { color: 'green' },
-      shape: 'dot',
-    },
-    edges: {
-      arrows: {
-        to: { enabled: false },
-      },
-      color: {
-        color: '#848484',
-        highlight: 'red',
-        hover: '#848484',
-        inherit: 'from',
-        opacity: 1.0,
-      },
-    },
-  }
+    }
 
-  const events = {
-    select: function (event) {
-      var { nodes, edges } = event
-    },
-  }
-
-  if (props.isOpen) {
-    return (
-      //<div className="test">{props.station}</div>
-      <Graph
-        graph={graph}
-        options={options}
-        events={events}
-        getNetwork={network => {
-          //  if you want access to vis.js network api you can set the state in a parent component using this property
-        }}
-      />
-    )
-  } else {
-    return null
+    if (this.props.isOpen) {
+      return (
+        <Graph
+          graph={this.state.graph}
+          options={this.options}
+          events={events}
+          getNetwork={network => (this.network = network)}
+        />
+      )
+    } else {
+      return null
+    }
   }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,6 +21,7 @@ export default function Home() {
   ]
 
   function toggleAboutText() {
+    console.log(graphVisible)
     setAboutText(!aboutText)
     setGraphVisible(!graphVisible)
   }
