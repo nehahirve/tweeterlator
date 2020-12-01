8ae8449e41f67d657ca6c6bef52be683cb78cf9f
nehahirve <neha.s.hirve@gmail.com>
2020-12-01 22:46:07 +0100

    created a database

---
+++ static/createstorage.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const cities = require('./seed.json')
+
+function saveTweets() {
+  let data
+  if (fs.existsSync(`database.json`)) {
+    data = JSON.parse(fs.readFileSync(`database.json`))
+  } else data = { lastFetched: Date.now(), fetchCount: 0, cities: {} }
+  for (let city of cities) {
+    let cityData = data.cities[city.name] || {}
+    for (let i = 0; i < 100; i++) {
+      let path = `JSONDATA/${city.name}_${i + 1}.json`
+      if (fs.existsSync(path)) {
+        let file = fs.readFileSync(path)
+        let tweets = JSON.parse(file).statuses
+        for (let tweet of tweets) {
+          const id = tweet.id
+          const text = tweet.full_text
+          const time = tweet.created_at
+          cityData[id] = { text, time }
+        }
+      }
+    }
+    data.cities[city.name] = cityData
+  }
+  data.fetchCount++
+  data.lastFetched = Date.now()
+  fs.writeFileSync(`database.json`, JSON.stringify(data))
+}
+
+saveTweets()
