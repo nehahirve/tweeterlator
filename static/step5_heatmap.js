3ee8f71d6c5bd682d1a13277806bfca564cb5c18
nehahirve <neha.s.hirve@gmail.com>
2020-12-02 12:21:45 +0100

    started getting time data

--- static/step5_heatmap.js
+++ static/step5_heatmap.js
@@ -1,42 +1,41 @@
 const fs = require('fs') // removes common words
-const cities = require('./seed.json')
+const cityNames = require('./seed.json')
 
-// const oldClock = fs.readFileSync('data_heatmap.json')
-const newClock = getDateData(cities)
-// const times = addnewClockToOldClock(newClock, oldClock)
+const database = JSON.parse(fs.readFileSync('database.json'))
 
-function addnewClockToOldClock() {
-  for (let city of cities) {
-    console.log(oldClock[city])
+const fetchCount = database.fetchCount
+
+function createClock(database) {
+  let clock = {}
+  for (let city of cityNames) {
+    cityName = city.name
+    let times = getTimes(cityName, database[cityName])
+    clock[cityName] = times
   }
-}
-
-fs.writeFileSync('data_heatmap_storage.json', JSON.stringify(newClock))
-
-function getDateData(citiesObject) {
-  let data = {}
-  for (let city of cities) {
-    let times = getDates(city)
-    data[city.name] = times
-  }
-  return data
-}
-
-function getDates(city) {
-  const clock = {}
-  for (let i = 0; i < 10; i++) {
-    let tweetFile = fs.readFileSync(`JSONDATA/${city.name}_${i + 1}.json`)
-    let tweets = JSON.parse(tweetFile).statuses
-    for (let tweet of tweets) {
-      let hour = +tweet.created_at.slice(11, 13)
-      if (clock[hour]) {
-        clock[hour]++
-      } else {
-        clock[hour] = 1
-      }
-    }
-  }
+  console.log(clock)
   return clock
 }
 
-// Tue Dec 01 12:50:45 +0000 2020
+function getTimes(cityName, cityObject) {
+  let hoursArray = new Array(24).fill(0)
+
+  for (let [key, value] of Object.entries(cityObject)) {
+    const hour = +value.time.slice(11, 13)
+    hoursArray[hour]++
+    console.log(cityName, hour)
+  }
+  hoursArray = hoursArray.map(hour => {
+    return Math.round(hour / fetchCount)
+  })
+  return hoursArray
+}
+
+let clock = createClock(database.cities)
+
+createSunburst(clock)
+
+function createSunburst(clock) {
+  let labels = cityNames.map(city => city.name)
+  console.log(labels)
+  let data = {}
+}
